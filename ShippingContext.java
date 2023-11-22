bbfc575e87dba528f389d1e7f9eef22ab330c5be
Mahdiyeh <mahdiyeh.ep@gmail.com>
2023-11-22 17:54:43 +0330

    strategy pattern handled

---
+++ ShippingContext.java
@@ -0,0 +1,12 @@
+class ShippingContext {
+    private ShippingStrategy shippingStrategy;
+    public ShippingContext(ShippingStrategy shippingStrategy) {
+        this.shippingStrategy = shippingStrategy;
+    }
+    public void setShippingStrategy(ShippingStrategy shippingStrategy) {
+        this.shippingStrategy = shippingStrategy;
+    }
+    public double calculateShippingPrice(double weight) {
+        return shippingStrategy.calculatePrice(weight);
+    }
+}
