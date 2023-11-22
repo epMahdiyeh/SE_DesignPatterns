aba6df984510cb83e9b606f62060691d7a551908
Mahdiyeh <mahdiyeh.ep@gmail.com>
2023-11-22 17:43:45 +0330

    TDD: test class added

---
+++ ShippingTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ShippingTest {
+    @Test
+    public void testStandard() {
+        ShippingStrategy standardShipping = new StandardShipping();
+        double price = standardShipping.calculatePrice(2.0);
+        assertEquals(5.0, price, 0.001);
+    }
+
+    @Test
+    public void testExpress() {
+        ShippingStrategy expressShipping = new ExpressShipping();
+        double price = expressShipping.calculatePrice(2.0);
+        assertEquals(7.0, price, 0.001);
+    }
+}
